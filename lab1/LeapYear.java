ba2e98d9bb86a4037f206ad6ae3e4354064c7695
ZJU-Winter <78242467+ZJU-Winter@users.noreply.github.com>
2021-12-23 15:16:17 +0800

    Finished Leapyear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,16 +1,33 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+/**
+ * Class that determines whether or not a year is a leap year.
+ * 
+ * @author YOUR NAME HERE
  */
 public class LeapYear {
-
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
+    /**
+     * Calls isLeapYear to print correct statement.
+     * 
+     * @param year to be analyzed
      */
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
         } else {
             System.out.printf("%d is not a leap year.\n", year);
+        }
+    }
+
+    /**
+     * return a boolean which tells if the year is a leapyear
+     * 
+     * @param year to be analyzed
+     * @return
+     */
+    public static boolean isLeapYear(int year) {
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -30,4 +47,3 @@ public class LeapYear {
         }
     }
 }
-
