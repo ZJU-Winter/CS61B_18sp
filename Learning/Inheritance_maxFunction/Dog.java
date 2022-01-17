b9b568b3c35e2c40ea5031037b2df41c4a2c0772
ZJU-Winter <78242467+ZJU-Winter@users.noreply.github.com>
2022-01-17 17:40:08 +0800

    Update Learning Files

---
+++ Learning/Inheritance_maxFunction/Dog.java
@@ -0,0 +1,18 @@
+public class Dog implements OurComparable {
+    private String name;
+    private int size;
+
+    public Dog(String n, int s) {
+        name = n;
+        size = s;
+    }
+
+    public void bark() {
+        System.out.println(name + " says: bark");
+    }
+
+    public int compareTo(Object o) {
+        Dog dog = (Dog) o;
+        return this.size - dog.size;
+    }
+}
---
+++ Learning/Inheritance_maxFunction/Maximizer.java
@@ -0,0 +1,22 @@
+import javax.crypto.spec.DESKeySpec;
+
+import org.jcp.xml.dsig.internal.dom.DOMPGPData;
+
+public class Maximizer {
+    public static OurComparable max(OurComparable[] items) {
+        int maxDex = 0;
+        for (int i = 0; i < items.length; i += 1) {
+            int cmp = items[i].compareTo(items[maxDex]);
+            if (cmp > 0) {
+                maxDex = i;
+            }
+        }
+        return items[maxDex];
+    }
+
+    public static void main(String[] args) {
+        Dog[] dogs = { new Dog("1", 10), new Dog("2", 15), new Dog("Max", 20) };
+        Dog maxDog = (Dog) Maximizer.max(dogs);
+        maxDog.bark();
+    }
+}
