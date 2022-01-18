51bbb30731d49562858aa9e6bb0d7d29b417c98d
ZJU-Winter <78242467+ZJU-Winter@users.noreply.github.com>
2022-01-18 11:39:35 +0800

    Finished Project 1B

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -7,6 +7,7 @@ public class OffByN implements CharacterComparator {
 
     @Override
     public boolean equalChars(char x, char y) {
-        return Math.abs(Character.toUpperCase(x) - Character.toUpperCase(y)) == N;
+        //return Math.abs(Character.toUpperCase(x) - Character.toUpperCase(y)) == N;
+        return Math.abs((int) x - (int) y) == N;
     }
 }
