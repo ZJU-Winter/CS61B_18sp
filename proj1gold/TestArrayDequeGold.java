e78200d141cd009229ba3dcd88fc01d6246628ee
ZJU-Winter <78242467+ZJU-Winter@users.noreply.github.com>
2022-01-19 17:12:43 +0800

    Added TestArrayDequeGold.java

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    StudentArrayDeque<Integer> studentDequeForTest = new StudentArrayDeque<>();
+    ArrayDequeSolution<Integer> dequeSolutionForTest = new ArrayDequeSolution<>();
+    Integer expectedRemovedLast, expectedRemovedFirst;
+    Integer actualRemovedLast, actualRemovedFirst;
+    String msg = "";
+    @Test
+    public void test () {
+        for(int i = 0; i < 100; i += 1) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            if (numberBetweenZeroAndOne < 0.25) {
+                studentDequeForTest.addFirst(i);
+                dequeSolutionForTest.addFirst(i);
+                msg += "addFirst"+'(' + i + ')' + '\n';
+            } else if (numberBetweenZeroAndOne < 0.5) {
+                studentDequeForTest.addLast(i);
+                dequeSolutionForTest.addLast(i);
+                msg += "addLast"+'(' + i + ')' + '\n';
+            } else if (numberBetweenZeroAndOne < 0.75) {
+                if(!studentDequeForTest.isEmpty() && !dequeSolutionForTest.isEmpty()) {
+                    actualRemovedFirst = studentDequeForTest.removeFirst();
+                    expectedRemovedFirst = dequeSolutionForTest.removeFirst();
+                    msg += "removeFirst()\n";
+                    assertEquals(msg ,expectedRemovedFirst,actualRemovedFirst);
+                }
+            } else {
+                    if(!studentDequeForTest.isEmpty() && !dequeSolutionForTest.isEmpty()) {
+                        actualRemovedLast = studentDequeForTest.removeLast();
+                        expectedRemovedLast = dequeSolutionForTest.removeLast();
+                        msg += "removeLast()\n";
+                        assertEquals(msg ,expectedRemovedLast,actualRemovedLast);
+                }
+            }
+        }
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
