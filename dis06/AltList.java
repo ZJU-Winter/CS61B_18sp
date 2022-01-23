e318845a60bdf5844fd9b80ef7df0a9a174076a9
ZJU-Winter <78242467+ZJU-Winter@users.noreply.github.com>
2022-01-23 14:14:01 +0800

    Added AltList.java

---
+++ dis06/AltList.java
@@ -0,0 +1,25 @@
+public class AltList<X, Y> {
+    private X item;
+    private AltList<Y, X> next;
+
+    AltList(X item, AltList<Y, X> next) {
+        this.item = item;
+        this.next = next;
+    }
+
+    public AltList<Y, X> pairsSwapped() {
+        AltList<Y, X> rst = new AltList<Y, X>(next.item, new AltList<X, Y>(item, null));
+        if (next.next != null) {
+            rst.next.next = next.next.pairsSwapped();
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        AltList<Integer, String> list = new AltList<Integer, String>(5,
+                new AltList<String, Integer>("cat",
+                        new AltList<Integer, String>(10,
+                                new AltList<String, Integer>("dog", null))));
+        AltList<String, Integer> res = list.pairsSwapped();
+    }
+}
