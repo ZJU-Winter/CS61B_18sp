7e9cd5923bacad0acf4ed006ef789a54e3bf369f
ZJU-Winter <78242467+ZJU-Winter@users.noreply.github.com>
2022-01-23 14:32:09 +0800

    Added AltList.java

--- dis06/AltList.java
+++ dis06/AltList.java
@@ -10,7 +10,20 @@ public class AltList<X, Y> {
     public AltList<Y, X> pairsSwapped() {
         AltList<Y, X> rst = new AltList<Y, X>(next.item, new AltList<X, Y>(item, null));
         if (next.next != null) {
-            rst.next.next = next.next.pairsSwapped();
+            rst.next.next = this.next.next.pairsSwapped();
+        }
+        return rst;
+    }
+
+    public AltList<Y, X> pairsSwappedIter() {
+        AltList<Y, X> rst = new AltList<Y, X>(next.item, new AltList<X, Y>(item, null));
+        AltList<X, Y> ptrOfOriginal = this.next.next;
+        AltList<Y, X> ptrOfResult = rst;
+        while (ptrOfOriginal != null) {
+            ptrOfResult.next.next = new AltList<Y, X>(ptrOfOriginal.next.item,
+                    new AltList<X, Y>(ptrOfOriginal.item, null));
+            ptrOfOriginal = ptrOfOriginal.next.next;
+            ptrOfResult = ptrOfResult.next.next;
         }
         return rst;
     }
@@ -20,6 +33,6 @@ public class AltList<X, Y> {
                 new AltList<String, Integer>("cat",
                         new AltList<Integer, String>(10,
                                 new AltList<String, Integer>("dog", null))));
-        AltList<String, Integer> res = list.pairsSwapped();
+        AltList<String, Integer> res = list.pairsSwappedIter();
     }
 }
