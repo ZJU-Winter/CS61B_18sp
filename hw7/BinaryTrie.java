973913571726d28964519c88be61468249ee8464
ZJU-Winter <78242467+ZJU-Winter@users.noreply.github.com>
2022-03-16 14:57:00 +0800

    Add BinaryTrie

---
+++ hw7/BinaryTrie.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+
+
+public class BinaryTrie {
+    public BinaryTrie(Map<Character, Integer> frequencyTable) {
+
+    }
+
+    public Match longestPrefixMatch(BitSequence querySequence) {
+        return null;
+
+    }
+
+    public Map<Character, BitSequence> buildLookupTable() {
+        return null;
+    }
+}
